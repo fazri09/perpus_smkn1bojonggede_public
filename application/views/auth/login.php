0f6a9e0bcb7230079c0a514663fc022c3092ea8d
Fazri <fazri.ramadhan@konekthing.com>
2025-05-14 05:10:53 +0700

    login rename

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -3,9 +3,8 @@
 
     <head>
         <meta charset="utf-8" />
-        <title>Login | Mifty - Admin & Dashboard Template</title>
+        <title>Login</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
         <meta content="" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
