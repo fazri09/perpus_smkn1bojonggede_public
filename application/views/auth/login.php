8a769f494d32838ec970a767410b7603aa18ddeb
Fazri <fazri.ramadhan@konekthing.com>
2025-05-14 05:16:54 +0700

    commit

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -3,7 +3,7 @@
 
     <head>
         <meta charset="utf-8" />
-        <title>Login F</title>
+        <title>Login</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta content="" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
