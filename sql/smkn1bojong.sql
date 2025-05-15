dd99d7e24709f67abcae9703cca11c522180c557
Fazri <fazri.ramadhan@konekthing.com>
2025-05-16 04:54:56 +0700

    sql

--- sql/smkn1bojong.sql
+++ sql/smkn1bojong.sql
@@ -40,26 +40,28 @@ CREATE TABLE `mst_jurusan` (
   `created_by` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `created_by` (`created_by`),
-  CONSTRAINT `mst_jurusan_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`),
-  CONSTRAINT `mst_jurusan_ibfk_2` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`)
+  CONSTRAINT `mst_jurusan_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `mst_jurusan` (`id`, `nama_jurusan`, `singkatan_jurusan`, `created_at`, `created_by`) VALUES
-(3,	'Rekayasa Perangkat Lunak',	'RPL',	'2025-05-15 03:16:16',	1);
+(3,	'Rekayasa Perangkat Lunak',	'RPL',	'2025-05-15 03:16:16',	1),
+(4,	'Teknik Komputer Jaringan',	'TKJ',	'2025-05-16 04:39:23',	1);
 
-DROP TABLE IF EXISTS `peminjam`;
-CREATE TABLE `peminjam` (
+DROP TABLE IF EXISTS `mst_kelas`;
+CREATE TABLE `mst_kelas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nis` int(11) DEFAULT NULL,
-  `nama` varchar(150) DEFAULT NULL,
-  `kelas` varchar(25) DEFAULT NULL,
-  `id_jurusan` int(11) DEFAULT NULL,
-  `no_hp` varchar(15) NOT NULL,
+  `nama_kelas` varchar(50) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `jurusan` (`id_jurusan`),
-  CONSTRAINT `peminjam_ibfk_1` FOREIGN KEY (`id_jurusan`) REFERENCES `mst_jurusan` (`id`)
+  KEY `created_by` (`created_by`),
+  CONSTRAINT `mst_kelas_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `mst_kelas` (`id`, `nama_kelas`, `created_at`, `created_by`) VALUES
+(1,	'XII RPL 1',	'2025-05-16 00:59:42',	1),
+(2,	'X RPL 2',	'2025-05-16 01:00:15',	1),
+(3,	'X RPL 1',	'2025-05-16 01:03:04',	1);
 
 DROP TABLE IF EXISTS `pos`;
 CREATE TABLE `pos` (
@@ -80,6 +82,30 @@ INSERT INTO `pos` (`id`, `buku_id`, `qty`, `type`, `created_by`) VALUES
 (3,	4,	20,	'IN',	1),
 (4,	6,	21,	'IN',	1);
 
+DROP TABLE IF EXISTS `siswa`;
+CREATE TABLE `siswa` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nis` bigint(50) DEFAULT NULL,
+  `nama` varchar(150) DEFAULT NULL,
+  `id_kelas` int(11) DEFAULT NULL,
+  `id_jurusan` int(11) DEFAULT NULL,
+  `no_hp` varchar(15) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `jurusan` (`id_jurusan`),
+  KEY `id_kelas` (`id_kelas`),
+  KEY `created_by` (`created_by`),
+  CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`id_jurusan`) REFERENCES `mst_jurusan` (`id`),
+  CONSTRAINT `siswa_ibfk_2` FOREIGN KEY (`id_kelas`) REFERENCES `mst_kelas` (`id`),
+  CONSTRAINT `siswa_ibfk_3` FOREIGN KEY (`id_jurusan`) REFERENCES `mst_jurusan` (`id`),
+  CONSTRAINT `siswa_ibfk_4` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `siswa` (`id`, `nis`, `nama`, `id_kelas`, `id_jurusan`, `no_hp`, `created_at`, `created_by`) VALUES
+(2,	9223372036854775807,	'Noer Fazri Ramadhann',	2,	4,	'08510103330',	'2025-05-16 04:50:17',	1),
+(3,	123131111222111,	'Fazri',	1,	3,	'08510103330',	'2025-05-16 04:46:25',	1);
+
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -94,6 +120,6 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `users` (`id`, `username`, `password`, `nama`, `last_login`, `last_ip`, `created_at`, `role`) VALUES
-(1,	'admin',	'$2y$10$catWtYE4u6kiNSdhzZLdJOXFMKlSrftYN4LzmIkVB4L6XXbz1CMlW',	'Admin',	'2025-05-15 03:49:43',	'::1',	'2025-05-14 22:20:46',	'admin');
+(1,	'admin',	'$2y$10$catWtYE4u6kiNSdhzZLdJOXFMKlSrftYN4LzmIkVB4L6XXbz1CMlW',	'Admin',	'2025-05-16 04:32:10',	'::1',	'2025-05-14 22:20:46',	'admin');
 
--- 2025-05-14 21:28:53
+-- 2025-05-15 21:54:26
