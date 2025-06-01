9d09e61383cadebc657d3456b01e1010db42f7ba
Fazri <fazri.ramadhan@konekthing.com>
2025-06-01 14:34:28 +0700

    MIFTY REMOVED

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -6,7 +6,7 @@
     </script>
 
     <meta charset="utf-8" />
-    <title><?= isset($title) ? $title : 'Dashboard'; ?> | Mifty</title>
+    <title><?= isset($title) ? $title : 'Dashboard'; ?></title>
     <link rel="shortcut icon" href="<?= base_url('assets/images/favicon.ico') ?>">
     <link href="<?= base_url('assets/libs/simple-datatables/style.css') ?>" rel="stylesheet" type="text/css" />
     <link href="<?= base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
